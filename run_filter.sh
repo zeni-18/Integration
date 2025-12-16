a0ae02ae34ad0753d31233a9320a58f571eebab0
naren <narendhran2006@gmail.com>
2025-12-16 09:27:05 +0530

    chore: archive author-rewrite script (remove identifiable names)

---
+++ run_filter.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Archived: author-rewrite completed. This script previously rewrote commit authors from 'Nithish-k' to 'naren'.
+# It is kept for audit purposes and no longer contains identifiable names.
+# If you need to run history rewrites again, recreate a safe script and review docs at https://github.com/newren/git-filter-repo
